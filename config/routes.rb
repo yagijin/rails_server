f6f406e636d24f0f370b6e29e0b15f1ccda3afa2
yagijin <jinjin.0327.jinjin@gmail.com>
2022-04-21 00:24:31 +0900

    update graphiqlの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  # developmentでgraphiqlを使用する
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
+  
+  # graphql
   post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
