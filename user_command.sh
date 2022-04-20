eb1bf0c61b509c9ac6fc69119bcdd559182cc1d1
yagijin <jinjin.0327.jinjin@gmail.com>
2022-04-21 00:12:11 +0900

    Revert "test deploy"
    This reverts commit 0411d3e0107da07dcd0da978432b55dc347a0554.

--- user_command.sh
+++
@@ -1,4 +0,0 @@
-bundle install --deployment --without development,test --path vendor/bundle
-bundle exec rails webpacker:install
-bundle exec rails webpacker:compile
-bundle exec rails assets:precompile
