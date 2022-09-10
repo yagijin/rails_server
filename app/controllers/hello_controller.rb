96851e7fca7abcf0c633dc81f37dc510a60cff6d
yagijin <yagijin_dev@ymail.ne.jp>
2022-09-10 14:53:02 +0900

    format rubocop -A

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HelloController < ApplicationController
   def index
     render json: { data: 'HelloWorld' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # graphqlのエンドポイント
   post '/graphql', to: 'graphql#execute'
